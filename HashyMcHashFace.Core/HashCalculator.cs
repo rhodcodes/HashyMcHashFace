a12f97b4c5a42da726b24f00e48a2a80ba351d3e
Rhodri Lloyd-Evans <rhodri.lloyd-evans@cp-ltd.co.uk>
2022-05-20 12:54:01 +0100

    Initial Commit v2

---
+++ HashyMcHashFace.Core/HashCalculator.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography;
+
+namespace HashyMcHashFace.Core
+{
+    public enum HashingAlgorithm
+    {
+        SHA256,
+        SHA384,
+        SHA512,
+        MD5,
+        SHA1
+    }
+
+    public class HashCalculator
+    {
+        private readonly HashAlgorithm _hashAlgorithm;
+
+        public HashCalculator(HashingAlgorithm hashingAlgorithm)
+        {
+            _hashAlgorithm = GetHashAlgorithm(hashingAlgorithm);
+        }
+
+        public string Calculate(FileInfo file)
+        {
+            using var stream = File.OpenRead(file.FullName);
+
+            var hash = _hashAlgorithm.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "");
+        }
+
+        public ICollection<string> Calculate(IEnumerable<FileInfo> files)
+        {
+            var hashes = new List<string>(files.Count());
+
+            foreach (var file in files)
+            {
+                hashes.Add(Calculate(file));
+            }
+
+            return hashes;
+        }
+
+        private HashAlgorithm GetHashAlgorithm(HashingAlgorithm hashingAlgorithm)
+        {
+            var algorithm = hashingAlgorithm switch
+            {
+                HashingAlgorithm.SHA256 => HashAlgorithm.Create("SHA256"),
+                HashingAlgorithm.SHA384 => HashAlgorithm.Create("SHA384"),
+                HashingAlgorithm.SHA512 => HashAlgorithm.Create("SHA512"),
+                HashingAlgorithm.MD5 => HashAlgorithm.Create("MD5"),
+                HashingAlgorithm.SHA1 => HashAlgorithm.Create("SHA1"),
+                _ => throw new ArgumentOutOfRangeException(nameof(hashingAlgorithm),
+                                                           "Cannot determine which hashing alorithm to use."),
+            };
+
+            ArgumentNullException.ThrowIfNull(algorithm);
+            return algorithm;
+        }
+    }
+}
